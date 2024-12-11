bca4ca5dfe58576c8cb43258bbbadbbe4c01dca0
oscar <oscar@111percent.net>
2024-12-11 23:06:21 +0900

    Problem2 Clear

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder answer = new StringBuilder(cryptogram);
+
+        boolean trigger = true;
+
+        while(trigger){
+            for(int i = 0; i < answer.length() - 1; ++i){
+                //같다면 제거
+                if(answer.charAt(i) == answer.charAt(i+1)){
+                    answer.deleteCharAt(i);
+                    answer.deleteCharAt(i+1);
+                }
+            }
+        }
+
+        return answer.toString();
     }
 }
