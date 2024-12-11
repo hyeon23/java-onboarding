03d12a5fb78fdfe898611ca7874b7c7d2c0ba936
oscar <oscar@111percent.net>
2024-12-11 23:12:54 +0900

    feat: Problem3 Clear

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int targetNum = 1; targetNum <= number; ++targetNum){
+            var targetStr = String.valueOf(targetNum);
+
+            for(int idx = 0; idx < targetStr.length(); ++idx){
+                if(targetStr.charAt(idx) == '3' || targetStr.charAt(idx) == '6' || targetStr.charAt(idx) == '9'){
+                    ++answer;
+                }
+            }
+        }
+
         return answer;
     }
 }
