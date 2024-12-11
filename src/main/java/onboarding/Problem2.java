ad5ddbc1c5e3a0ca69cb22717130b7d554cf1013
oscar <oscar@111percent.net>
2024-12-11 23:20:49 +0900

    아 실행하는 방법을 잘못알았다... 1, 2, 3번 문제 재커밋

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,17 +4,22 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         StringBuilder answer = new StringBuilder(cryptogram);
 
-        boolean trigger = true;
+        boolean trigger;
 
-        while(trigger){
+        do{
+            trigger = false;
+
             for(int i = 0; i < answer.length() - 1; ++i){
                 //같다면 제거
                 if(answer.charAt(i) == answer.charAt(i+1)){
-                    answer.deleteCharAt(i);
                     answer.deleteCharAt(i+1);
+                    answer.deleteCharAt(i);
+
+                    trigger = true;
                 }
             }
-        }
+
+        }while(trigger);
 
         return answer.toString();
     }
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        String origin = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+        String dict = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+
+        for(int i = 0; i < word.length(); ++i) {
+
+            if(word.charAt(i) == ' '){
+                answer += ' ';
+            }
+            else{
+                char targetChar = dict.charAt(origin.indexOf(word.charAt(i)));
+                answer += targetChar;
+            }
+        }
+
         return answer;
     }
 }
