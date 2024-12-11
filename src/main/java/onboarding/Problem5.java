412bbb7da84b847fa4b0d0fcae1230db055f990b
oscar <oscar@111percent.net>
2024-12-12 00:00:19 +0900

    Problem5 Clear 아니 배열 뭐 이따위로 써야하냐..

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        int[] currencies = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        int curMoney = money;
+
+        for(int i = 0; i < currencies.length; ++i){
+
+            Integer temp = curMoney / currencies[i];
+            answer.add(temp);
+            curMoney %= currencies[i];
+        }
+
         return answer;
     }
 }
